a7a8c22fd3f785a2c694035adde801aeb56d983f
Jose Gutierrez Marin <andres.gutierrez-m@mail.escuelaing.edu.co>
2021-02-02 15:17:48 -0500

    tests

---
+++ src/main/java/edu/escuelaing/arep/heroku/app/calculator/Reader.java
@@ -0,0 +1,47 @@
+package edu.escuelaing.arep.heroku.app.calculator;
+
+import java.io.BufferedReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.*;
+
+/**
+ * @author Jose Gutierrez Marin
+ */
+public class Reader {
+	
+	/***
+	 * Reader's builder
+	 */
+	public Reader () {
+	}
+	
+	/***
+	 * Reads the specified file
+	 * @param file The file's path address
+	 * @return Returns a matrix with the data in the file
+	 */
+	public List<List<String>> readFile(String file) {
+		List<List<String>> data = new ArrayList<List<String>>();
+		try {
+			FileReader fr = new FileReader (file);
+			BufferedReader b = new BufferedReader(fr);
+			String x =  "Joker";
+			while (x != null) {
+				data.add(Arrays.asList(x.split(" ")));
+				x = b.readLine();
+			}
+			data.remove(0);
+		}catch(Exception e) {
+			System.out.println("Something went wrong");
+			System.out.println(e);
+		}
+		return data;
+		
+		
+	}
+}
--- src/test/java/edu/escuelaing/arep/heroku/app/AppTest.java
+++ src/test/java/edu/escuelaing/arep/heroku/app/AppTest.java
@@ -1,38 +1,118 @@
 package edu.escuelaing.arep.heroku.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import edu.escuelaing.arep.heroku.app.calculator.Reader;
+import edu.escuelaing.arep.heroku.app.calculator.Calculator;
+import edu.escuelaing.arep.heroku.app.calculator.JoseLinkedList;
+import edu.escuelaing.arep.heroku.app.calculator.Node;
 
 /**
- * Unit test for simple App.
+ * @author Jose Gutierrez Marin
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+   @Test
+    public void shouldReadTheTestFile() {
+    	Reader r = new Reader();
+    	List<List<String>> answ = new ArrayList<List<String>>();
+    	List<String> temp = new ArrayList<String>();
+    	temp.add("primera");
+    	temp.add("linea");
+    	answ.add(temp);
+    	temp.remove(0);
+    	temp.remove(0);
+    	temp.add("segunda");
+    	temp.add("linea");
+    	answ.add(temp);
+    	temp.remove(0);
+    	temp.remove(0);
+    	temp.add("linea");
+    	temp.add("tercera");
+    	answ.add(temp);
+    	temp.remove(0);
+    	temp.remove(0);
+    	temp.add("fin");
+    	temp.add("lineas");
+    	answ.add(temp);
+    	List<List<String>> data = r.readFile("testFile.txt");
+    	
+    	int i = 0;
+    	for (List<String> row : data) {
+    		answ.get(i).get(0).equals(data.get(0));
+    		answ.get(i).get(1).equals(data.get(1));
+    		i++;
+    	}
+    	
     }
+    
+    @Test
+    public void shouldAddNodesInJoseLinkedList() {
+    	JoseLinkedList jl = new JoseLinkedList();
+    	jl.addFirst(new Node(1));
+    	jl.addFirst(new Node(2));
+    	jl.addFirst(new Node(3));
+    	jl.addFirst(new Node(4));
+    	jl.addFirst(new Node(5));
+    }
+    
+    @Test
+    public void shouldReturnCorrectNodesFromJoseLinkedList() {
+    	JoseLinkedList jl = new JoseLinkedList();
+    	jl.addFirst(new Node(1));
+    	assertTrue(1==jl.getFirst().getValue());
+    	jl.addFirst(new Node(2));
+    	assertTrue(1==jl.getFirst().getValue());
+    	assertTrue(2==jl.getLast().getValue());
+    	jl.addFirst(new Node(3));
+    	assertTrue(1==jl.getFirst().getValue());
+    	assertTrue(3==jl.getLast().getValue());
+    	jl.addFirst(new Node(4));
+    	assertTrue(1==jl.getFirst().getValue());
+    	assertTrue(4==jl.getLast().getValue());
+    	jl.addFirst(new Node(5));
+    	assertTrue(1==jl.getFirst().getValue());
+    	assertTrue(5==jl.getLast().getValue());
+    }
+    
+    
+    @Test
+    public void JoseLinkedListShouldBeArranged() {
+    	JoseLinkedList jl = new JoseLinkedList();
+    	jl.addFirst(new Node(1));
+    	assertTrue(1==jl.getFirst().getValue());
+    	assertTrue(1==jl.getFirst().getValue());
+    	jl.addFirst(new Node(2));
+    	assertTrue(1==jl.getFirst().getValue());
+    	assertTrue(2==jl.getLast().getValue());
+    	assertTrue(null==jl.getLast().getNext());
+    	assertTrue(2==jl.getFirst().getNext().getValue());
+    	jl.addFirst(new Node(3));
+    	assertTrue(1==jl.getFirst().getValue());
+    	assertTrue(3==jl.getLast().getValue());
+    	assertTrue(null==jl.getLast().getNext());
+    	assertTrue(2==jl.getFirst().getNext().getValue());
+    
+    }
+	
+	@Test
+    public void shouldReturnTheCorrectAnswer() {
+		Calculator c = new Calculator();
+		Reader r = new Reader();
+    	JoseLinkedList jl = new JoseLinkedList();
+    	List<List<String>> data = r.readFile("archivo.txt");
+		for (int i = 0; i < data.size(); i++){
+			jl.addFirst(new Node(Double.parseDouble(data.get(i).get(0))));
+		}
+		assertTrue(c.calculateMeanOf(jl) == 550.6);
+		assertTrue(c.calculateStandardDerivationOf(jl, 550.6) == 572.03 );
+		
+    }
+    
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    
 }
